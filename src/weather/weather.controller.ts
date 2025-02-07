655dff6e58d3ab0af71f077a4a8875c32b2077fc
Rishabh Porwal <rishabhporwal95@gmail.com>
2025-02-08 00:14:40 +0530

    feature: Code Setup and Weather service creation

---
+++ src/weather/weather.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { WeatherService } from './weather.service';
+
+@Controller('weather')
+export class WeatherController {
+  constructor(private readonly weatherService: WeatherService) {}
+
+  @Get(':city')
+  async getWeather(@Param('city') city: string) {
+    return await this.weatherService.getWeather(city);
+  }
+
+  @Get('forecast/:city')
+  async getForecast(@Param('city') city: string) {
+    return await this.weatherService.getForecast(city);
+  }
+}
